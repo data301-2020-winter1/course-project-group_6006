7695e2e871b5ddb39d999499890cfd3f99397cc1
stephham <hamilton.2016@alumni.ubc.ca>
2020-11-11 13:57:55 -0800

    Changed parameters in load_and_process function to include separator. Trying to use load_and_process function

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-def load_and_process(url_or_path_to_csv_file):
+# seperator for our data files should be ";" but for normal csv's it's ","
+
+def load_and_process(url_or_path_to_csv_file, seperator):
     
     # Method Chain (Load data and deal with missing data)
     df = (
-        pd.read_csv(url_or_path_to_csv_file)
+        pd.read_csv(url_or_path_to_csv_file, sep=seperator)
         .rename(columns={"fixed acidity": "fa", "volatile acidity": "va", "citric acid": "ca", 
                          "residual sugar": "rs", "free sulfur dioxide": "fsd", "total sulfur dioxide": "tsd"})
         .drop_duplicates()
